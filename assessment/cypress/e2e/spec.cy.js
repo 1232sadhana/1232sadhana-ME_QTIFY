ce6542824be8f7d0d538eba732b1a23e03edce4f
CrioUser <crio-user@criodo.com>
2024-01-05 19:17:37 +0530

    Test commit

--- assessment/cypress/e2e/spec.cy.js
+++ assessment/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe("Qtify Automation", () => {
   beforeEach(() => {
-    cy.visit("https://crio-1232sadhana-me-qtify-2cdo4klzl-sadhana-jadhavs-projects.vercel.app")
+    cy.visit("https://crio-1232sadhana-me-qtify-eh8kaoz6v-sadhana-jadhavs-projects.vercel.app")
   });
   describe("Navbar", () => {
     it("Should contain logo, search bar, and a button", () => {
--- assessment/runCypress.js
+++ assessment/runCypress.js
@@ -1,6 +1,6 @@
-const cypress = require('cypress');
-const fs = require('fs').promises;
-const dotenv = require('dotenv');
+import cypress from 'cypress';
+import { promises as fsPromises } from 'fs';
+import dotenv from 'dotenv';
 
 dotenv.config();
 console.log(process.env.USER_LINK_SUBMISSION);
@@ -8,14 +8,14 @@ console.log(process.env.USER_LINK_SUBMISSION);
 async function runCypress() {
   try {
     const filePath = './cypress/e2e/spec.cy.js';
-    let fileContent = await fs.readFile(filePath, 'utf8');
+    let fileContent = await fsPromises.readFile(filePath, 'utf8');
 
-    const updatedContent = fileContent.replace("https://crio-1232sadhana-me-qtify-2cdo4klzl-sadhana-jadhavs-projects.vercel.app", process.env.USER_LINK_SUBMISSION);
-
-    await fs.writeFile(filePath, updatedContent);
+    const updatedContent = fileContent.replace("https://crio-1232sadhana-me-qtify-eh8kaoz6v-sadhana-jadhavs-projects.vercel.app", process.env.USER_LINK_SUBMISSION);
+    
+    await fsPromises.writeFile(filePath, updatedContent);
 
     const results = await cypress.run();
-    await fs.writeFile('cypressResults.json', JSON.stringify(results, null, 2));
+    await fsPromises.writeFile('cypressResults.json', JSON.stringify(results, null, 2));
 
     console.log('Cypress tests completed successfully.');
   } catch (error) {
